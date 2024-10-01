23c52b2ab261280d244547a661a5e608e94c6cf6
FrankC-12 <facaicedoh@gmail.com>
2024-10-01 18:05:35 -0400

    [ADD] Docker

--- Backend/firebase_config.py
+++ Backend/firebase_config.py
@@ -24,7 +24,7 @@ import json
 # Cargar la ruta de las credenciales desde la variable de entorno
 firebase_credentials_path = os.environ.get("FIREBASE_CREDENTIALS")
 
-if firebase_credentials_path:
+if firebase_credentials_path and os.path.exists(firebase_credentials_path):
     # Leer las credenciales desde el archivo
     cred = credentials.Certificate(firebase_credentials_path)
     firebase_admin.initialize_app(cred)
