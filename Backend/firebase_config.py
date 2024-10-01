4d7f0a72e3e31c55137e1b82ac3a6c71b387f0a8
FrankC-12 <facaicedoh@gmail.com>
2024-10-01 17:36:27 -0400

    [ADD] Docker

--- Backend/firebase_config.py
+++ Backend/firebase_config.py
@@ -21,14 +21,32 @@ from firebase_admin import credentials, firestore
 import os
 import json
 
-# Cargar las credenciales desde la variable de entorno
-firebase_credentials = os.environ.get("FIREBASE_CREDENTIALS")
+# Cargar la ruta de las credenciales desde la variable de entorno
+firebase_credentials_path = os.environ.get("FIREBASE_CREDENTIALS")
 
-if firebase_credentials:
-    cred_dict = json.loads(firebase_credentials)
-    cred = credentials.Certificate(cred_dict)
+if firebase_credentials_path and os.path.exists(firebase_credentials_path):
+    # Leer las credenciales desde el archivo
+    cred = credentials.Certificate(firebase_credentials_path)
     firebase_admin.initialize_app(cred)
 else:
-    raise ValueError("Firebase credentials not found in environment variables")
+    raise ValueError("Firebase credentials file not found or path not set in environment variables")
 
+# Inicializar Firestore
 db = firestore.client()
+
+# import firebase_admin
+# from firebase_admin import credentials, firestore
+# import os
+# import json
+
+# # Cargar las credenciales desde la variable de entorno
+# firebase_credentials = os.environ.get("FIREBASE_CREDENTIALS")
+
+# if firebase_credentials:
+#     cred_dict = json.loads(firebase_credentials)
+#     cred = credentials.Certificate(cred_dict)
+#     firebase_admin.initialize_app(cred)
+# else:
+#     raise ValueError("Firebase credentials not found in environment variables")
+
+# db = firestore.client()
